03cf322b9cf5c9aca006a62010935beba6eacb33
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-05-26 23:32:25 +0500

    useState using functional-based component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
   return (
     <div className="App">
-      
+      <ClassCounter />
     </div>
   );
 }
